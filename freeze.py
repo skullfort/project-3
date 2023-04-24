7fa334b6b57527b663414e49ea0ee39f01662e09
skullfort <gumingyao@gmail.com>
2023-04-24 16:07:56 -0400

    Finalize index.html

--- freeze.py
+++ freeze.py
@@ -2,6 +2,6 @@ from flask_frozen import Freezer
 from app import app
 
 if __name__ == '__main__':
-    app.config["FREEZER_BASE_URL"] = 'https://skullfort.github.io/project-3-demo/'
+    app.config["FREEZER_BASE_URL"] = 'https://skullfort.github.io/bst-demo/'
     freezer = Freezer(app)
     freezer.freeze()
